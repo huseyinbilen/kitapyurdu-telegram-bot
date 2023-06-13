1f04f63155734a5156bbe8c08247be720f4094df
Huseyin Bilen <huseyinxbilen@gmail.com>
2023-06-13 21:27:02 +0300

    New Releases Added

--- scrapper.py
+++ scrapper.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 
-def mostSeller():
+def bestSellers():
     url = "https://www.kitapyurdu.com/cok-satan-kitaplar/haftalik/1.html"
 
     html = requests.get(url).content
@@ -20,5 +20,22 @@ def mostSeller():
         # print(f"{index}. {name}")
     return results
 
-result = mostSeller()
-print(result)
+def newReleases():
+    url = "https://www.kitapyurdu.com/index.php?route=product/best_sellers&list_id=2"
+
+    html = requests.get(url).content
+    soup = BeautifulSoup(html, "html.parser")
+
+    div = soup.find("div", {"class":"box no-padding"})
+    div = div.find_all("div", {"class": "product-cr"})
+
+    results = []
+
+    for index, item in enumerate(div, start=1):
+        name = item.find("div", {"class": "name"}).find("span").text
+        results.append(f"{index}. {name}")
+        # print(f"{index}. {name}")
+    return results
+
+results = newReleases()
+print(results)
